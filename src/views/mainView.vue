89f905384d6c077a35910d2ddda4ee4f5dbd51d3
jxaovito <joaovitoorvieiraborba@gmail.com>
2024-01-09 23:10:25 -0300

    structure, grids & navbar in progress

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -1,13 +1,28 @@
 <script setup>
+// let previousPosition = document.querySelector('.navbar').height() * 2;
 
+// window.addEventListener('scroll', function() {
+//   const currentPosition = document.querySelector('.navbar').height() * 2;
+
+//   if (previousPosition > currentPosition) {
+//     // user has scrolled up
+//     document.querySelector('.navbar').classList.add('show');
+//   } else {
+//     // user has scrolled down
+//     document.querySelector('.navbar').classList.remove('show');
+//   }
+
+//   // update previous scroll position
+//   previousPosition = currentPosition;
+// });
 </script>
 
 <template>
-<header class="sticky z-50 w-full">
-  <nav class="top-0 border-gray-200">
-    <div class="flex justify-between w-screen items-center p-4">
-        <img src="../img/kamigami.png" class="h-9 ml-10 mt-10" alt="KamiGami Logo" />
-        <div class="flex items-center space-x-6 rtl:space-x-reverse mr-10 mt-10">
+<header class="sticky top-0  z-50 w-full">
+  <nav class="relative navbar">
+    <div class="flex justify-between items-center p-4">
+        <img src="../img/kamigami.png" class="h-9 ml-10 mt-8" alt="KamiGami Logo" />
+        <div class="flex items-center space-x-6 rtl:space-x-reverse mr-10 mt-8">
             <a href="" class="font-montserrat font-light  text-gray-500 dark:text-white hover:text-red-400">Nosso Cardápio</a>
             <a href="" class="font-montserrat font-light   text-gray-500 dark:text-white hover:text-red-400">Faça seu pedido</a>
             <a href="" class="font-montserrat font-light  text-gray-500 dark:text-white hover:text-red-400">Sobre nós</a>
@@ -23,18 +38,31 @@
        <img alt="Vue logo" class="logo" src="../img/sushis.png"/>
   </div>
   
-  <div class="grid grid-rows-2">
+  <div class="">
       <span class="presentationSubtitle">GOSTARIA DE CONHECER OS NOSSOS</span>
       <h1 class="presentationTitle">INCRÍVEIS PRODUTOS?</h1>  
-      <div>
+      <div class="">
         <img class="sushi" src="../img/sushipresentation1.jpg" alt="">
         <img class="sushi1" src="../img/sushipresentation2.jpg" alt="">
         <img class="sushi2" src="../img/sushipresentation3.jpg" alt="">
-      </div>
-      <div>
         <h3>MAKI ROLLS</h3>
-      </div>
+    </div>
       </div>
       
+  <div>
+    <img src="" alt="">
+    <span></span>
+  </div>    
+
+
+
+
+      <div>
+      <h1 class="mt-10 presentationTitle">FAÇA SEU PEDIDO</h1>  
+      <div class="flex">
+        <span>Kamigami Sushi Bar, Av. Cel. Procópio Gomes, 918 - Bucarein, Joinville - SC, 89202-422</span>
+        <img class="w-1/3" src="../img/sushipresentation1.jpg" alt="">
+      </div>
+      </div>
 </template>
 
