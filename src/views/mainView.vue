227dd3824df2d6879f40fb301d5ded481034b722
jxaovito <joaovitoorvieiraborba@gmail.com>
2024-01-11 22:13:31 -0300

    responsividade e nova sessão de pedidos

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -1,32 +1,32 @@
 <script setup>
-// let previousPosition = document.querySelector('.navbar').height() * 2;
+let previousPosition = document.querySelector('.navbar').height() * 2;
 
-// window.addEventListener('scroll', function() {
-//   const currentPosition = document.querySelector('.navbar').height() * 2;
+window.addEventListener('scroll', function() {
+  const currentPosition = document.querySelector('.navbar').height() * 2;
 
-//   if (previousPosition > currentPosition) {
-//     // user has scrolled up
-//     document.querySelector('.navbar').classList.add('show');
-//   } else {
-//     // user has scrolled down
-//     document.querySelector('.navbar').classList.remove('show');
-//   }
+  if (previousPosition > currentPosition) {
+    // user has scrolled up
+    document.querySelector('.navbar').classList.add('show');
+  } else {
+    // user has scrolled down
+    document.querySelector('.navbar').classList.remove('show');
+  }
 
-//   // update previous scroll position
-//   previousPosition = currentPosition;
-// });
+  // update previous scroll position
+  previousPosition = currentPosition;
+});
 </script>
 
 <template>
   <header class="sticky top-0 z-50 w-full">
     <nav class="relative navbar">
-      <div class="flex justify-between items-center p-4">
+      <div class="flex justify-between items-center p-3">
         <img
           src="../img/kamigami.png"
-          class="h-9 ml-10 mt-8"
+          class="xl:h-8 xl:ml-12 xl:mt-6"
           alt="KamiGami Logo"
         />
-        <div class="flex items-center space-x-6 rtl:space-x-reverse mr-10 mt-8">
+        <div class="flex items-center space-x-6 xl:mr-12 xl:mt-6 xl:to-white">
           <a
             href=""
             class="font-montserrat font-light text-gray-500 dark:text-white hover:text-red-400"
@@ -53,13 +53,13 @@
   </header>
   <div class="">
     <span class="subtitle">VENHA EXPERIMENTAR A</span>
-    <h1 class="mainTitle">ESPECIALIDADE DA CASA</h1>
-    <img alt="Vue logo" class="logo" src="../img/sushis.png" />
+    <h1 class="mainTitle xl:mb-[-10rem]">ESPECIALIDADE DA CASA</h1>
+    <img alt="Vue logo" class="logo xl:w-[35rem] xl:pt-[-20rem]" src="../img/fotoprincipalsemciruclo.png" />
   </div>
 
   <div class="">
-    <span class="presentationSubtitle">GOSTARIA DE CONHECER OS NOSSOS</span>
-    <h1 class="presentationTitle">INCRÍVEIS PRODUTOS?</h1>
+    <span class="presentationSubtitle">DÊ UMA OLHADA NOS NOSSOS</span>
+    <h1 class="presentationTitle">PRINCIPAIS PRODUTOS</h1>
   </div>
 
   <div class="grid grid-cols-2">
@@ -70,9 +70,8 @@
     </div>
 
     <div class="products grid grid-rows-4">
-      <!-- <ul class="products"> -->
       <div class="grid grid-cols-2">
-        <h3 class="font-montserrat font-bold text-blue-500">MAKI ROLLS</h3>
+        <h3 class="font-montserrat font-bold text-blue-500 2xl:w-50">MAKI ROLLS</h3>
         <p>Um pequeno segmento cortado de um longo rolo de arroz e vários outros ingredientes embrulhados em uma folha de alga marinha.</p>
       </div>
       <div class="grid grid-cols-2">
@@ -88,22 +87,41 @@ Sashimi é uma iguaria japonesa que consiste em peixe bem fresco cortado em peda
         <h3 class="font-montserrat font-bold text-blue-500">NIGIRI</h3>
         <p>Sushi composto por uma pequena bola de arroz com um toque de wasabi e coberto com peixe cru ou outros frutos do mar.</p>
       </div>
-      <a href="#" class="btn btn-primary">Cardápio completo</a>
-      <!-- </ul> -->
+      <a href="#" class="btn btn-primary">Cardápio</a>
     </div>
   </div>
 
-  <div>
-    <img src="" alt="" />
-    <span></span>
+  <div class="xl:mt-[-13rem] xl:mb-[-1rem] 2xl:mt-[0rem] 2xl:ml-[6rem] orderContainer">
+    <span class="orderSubtitle xl:ml-[16rem]">ENCOMENDE AGORA</span>
+  <h1 class="orderTitle xl:ml-[9rem] xl:mt-[-0.5rem]">O SEU PEDIDO</h1>
+  
+</div>
+    <div class="infos">
+      <div class="grid grid-cols-2">
+        <h3 class="font-montserrat font-bold text-blue-500 xl:pl-[3rem] ">ENDEREÇO</h3>
+        <p>Kamigami Sushi Bar, Av. Cel. Procópio Gomes, 918 - Bucarein, Joinville - SC, 89202-422</p>
+      </div>
+      <div class="grid grid-cols-2">
+        <h3 class="font-montserrat font-bold text-blue-500">CONTATO</h3>
+        <p>Whatsapp: (47) 988140193
+          <br>Instagram: @sushikamigami
+        </p>
+      </div>
+      <div class="grid grid-cols-2">
+        <h3 class="font-montserrat font-bold text-blue-500">SASHIMI</h3>
+        <p>
+Sashimi é uma iguaria japonesa que consiste em peixe bem fresco cortado em pedaços finos.</p>
+      </div>
+      <div class="grid grid-cols-2">
+        <h3 class="font-montserrat font-bold text-blue-500">NIGIRI</h3>
+        <p>Sushi composto por uma pequena bola de arroz com um toque de wasabi e coberto com peixe cru ou outros frutos do mar.</p>
+
+      </div>
+     
+     
+    </div>
+    <div class="w-full">
+    <img class="xl:mt-[-43rem] xl:ml-[50rem] 2xl:ml-[60.7rem] 2xl:mt-[-45rem]" src="../img/bigsashimi.png" alt="" />
   </div>
 
-  <h1 class="mt-10 presentationTitle">FAÇA SEU PEDIDO</h1>
-  <div class="grid grid-cols-2">
-    <span
-      >Kamigami Sushi Bar, Av. Cel. Procópio Gomes, 918 - Bucarein, Joinville -
-      SC, 89202-422</span
-    >
-    <img class="w-1/2" src="../img/sushipresentation1.jpg" alt="" />
-  </div>
 </template>
